01101c922ad3093296acae2271f8f250da1a073d
Samuele Maci <macisamuele@gmail.com>
2022-12-04 22:04:27 +0100

    Use clap as CLI parser and update template to use 2021 edition and specificy anyhow version used

--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -1,8 +1,8 @@
+use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use structopt::StructOpt;
 
-#[derive(StructOpt, Debug)]
+#[derive(Parser, Debug)]
 struct Opts {
     /// Sets a custom input file for the assignment. Passing no value implies the usage of the "official" assignment input.
     // #[clap(short, long)]
@@ -23,9 +23,9 @@ fn read_lines(filename: &str) -> std::io::Result<Box<dyn Iterator<Item = String>
 /// Will return `Err` if the `filename` provided in the CLI argumens does not exist
 /// or the user does not have permission to read it.
 pub fn input_lines(input: &str) -> anyhow::Result<Vec<String>> {
-    let mut res: Vec<String> = match Opts::from_args().input_filename {
+    let mut res: Vec<String> = match Opts::parse().input_filename.as_deref() {
         Some(path) => {
-            eprintln!("Loading input from custom file: {}", path);
+            eprintln!("Loading input from custom file: {}", &path);
             read_lines(&path)?.collect()
         }
         None => input.split('\n').map(ToString::to_string).collect(),
